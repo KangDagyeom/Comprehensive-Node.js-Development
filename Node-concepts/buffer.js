31b82c07ae4b7f189bd5c7da0dbecc7fc338949c
hungndweq <thang7781@gmail.com>
2025-01-14 16:44:59 +0700

    Buffer and stream

--- Node-concepts/buffer.js
+++ Node-concepts/buffer.js
@@ -8,4 +8,15 @@ const buffFromArrayOfInteger = Buffer.from([1, 2, 3, 4, 5, 0]);
 console.log(buffFromArrayOfInteger)
 
 buffOne.write("Nodejs")
-console.log('After writing Nodejs to buffOne', buffOne.toString);
+console.log('After writing Nodejs to buffOne', buffOne.toString());
+
+console.log(buffFromString[0]);
+
+console.log(buffFromString.slice(0,3));
+
+const concatBuffs = Buffer.concat([buffOne,buffFromString]);
+console.log(concatBuffs);
+
+console.log(concatBuffs.toJSON());
+
+
