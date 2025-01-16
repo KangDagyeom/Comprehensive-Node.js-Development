26e288e6c3c9e1dfcd0dde8fb84b7bdbe38999cc
hungndweq <thang7781@gmail.com>
2025-01-16 23:42:47 +0700

    Comment in code

--- Express-concepts/middleware/errorHandler.js
+++ Express-concepts/middleware/errorHandler.js
@@ -7,10 +7,12 @@ class APIError extends Error {
     }
 }
 
+// Handling async error
 const asyncHandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next)
 }
 
+// The global error handler function
 const globalErrorHandler = (err, req, res, next) => {
     console.error(err.stack);
 
